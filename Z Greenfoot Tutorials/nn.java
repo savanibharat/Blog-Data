480b06a7537e90beefca47dcbd145df68db7dd76
BHARAT SAVANI <savanibharat@gmail.com>
2015-05-30 09:41:06 -0700

    Collection Interview Question 1 Explain ArrayList

---
+++ Z Greenfoot Tutorials/nn.java
@@ -0,0 +1,23 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class nn here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class nn extends Actor
+{
+    /**
+     * Act - do whatever the nn wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+        System.out.println("a");
+    }    
+    public static void main(String args[]){
+        System.out.println("aaaa");
+    }
+}
